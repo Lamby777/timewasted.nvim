a7e104d90ae8b22c0631b9a685a5fc5a649f5a81
Cherry <cherry@sparklet.org>
2023-09-04 00:25:00 -0400

    Main "get" functions done

--- lua/timewasted/init.lua
+++ lua/timewasted/init.lua
@@ -2,10 +2,13 @@ local M = {}
 
 -- File path to store the session time
 local session_time_file = vim.fn.stdpath("data") .. "/timewasted"
-local TIME_FMT = "%01dd %01dh %01dm %01ds"
+local TIME_FMT = "Time Wasted Configuring: %01dd %01dh %01dm %01ds"
 local AUTOSAVE_DELAY = 60
+local start_time = os.time()
 
-M.start_time = os.time()
+M.get_fmt = function()
+	return M.fmt_time(M.get_time())
+end
 
 M.fmt_time = function(total_sec)
 	local days = math.floor(total_sec / 86400)
@@ -16,24 +19,28 @@ M.fmt_time = function(total_sec)
 	return string.format(TIME_FMT, days, hours, minutes, seconds)
 end
 
+function M.get_time()
+	return M.read_log() + (os.time() - start_time)
+end
+
 -- write the logged time and reset the start time
-function M.write_time()
+function M.write_log()
 	local current_time = os.time()
-	local elapsed_seconds = current_time - M.start_time
-	local new_total = M.read_time() + elapsed_seconds
+	local elapsed_seconds = current_time - start_time
+	local new_total = M.read_log() + elapsed_seconds
 
 	local f = io.open(session_time_file, "w")
 	if f then
 		f:write(tostring(new_total))
 		f:close()
-		M.start_time = current_time
+		start_time = current_time
 	else
 		error("Unable to write time to file: " .. session_time_file)
 	end
 end
 
 -- read the logged time
-function M.read_time()
+function M.read_log()
 	local f = io.open(session_time_file, "r")
 	if f then
 		local logged = tonumber(f:read("*a")) or 0
@@ -45,7 +52,7 @@ function M.read_time()
 end
 
 -- create open/close autocmds
-vim.api.nvim_create_autocmd("VimEnter", "*", [[lua require("timewasted").load_time()]])
-vim.api.nvim_create_autocmd("VimLeave", "*", [[lua require("timewasted").write_time()]])
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { command = [[lua require("timewasted").load_time()]] })
+vim.api.nvim_create_autocmd({ "VimLeave" }, { command = [[lua require("timewasted").write_time()]] })
 
 return M
